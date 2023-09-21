d146757c9fa0d0b48c0f38e1bb5083a95cb18386
Trevor Harmon <tharmon@laspositascollege.edu>
2023-09-20 17:09:33 -0700

    Add solution (part 1)

--- src/circular_buffer.h
+++ src/circular_buffer.h
@@ -1,30 +1,61 @@
 #pragma once
 
 #include <ostream>
+#include <memory>
+#include <stdexcept>
 
 template <typename T>
 class CircularBuffer {
 private:
+    int maxSize;
+    std::unique_ptr<T[]> array;
+    int length;
 public:
     CircularBuffer(int maxSize) {
+        this->maxSize = maxSize;
+        array = std::make_unique<T[]>(maxSize);
+        length = 0;
     }
 
     int getSize() const {
-        return 0;
+        return length;
     }
 
     void insert(int index, T element) {
+        if (index < 0 || index > length || length == maxSize) {
+            throw std::out_of_range("Cannot insert");
+        }
+        for (int i = length - 1; i >= index; i--) {
+            array[i + 1] = array[i];
+        }
+        array[index] = element;
+        length++;
     }
 
     void remove(int index) {
+        if (index < 0 || index >= length) {
+            throw std::out_of_range("Cannot remove");
+        }
+        for (int i = index + 1; i < length; i++) {
+            array[i - 1] = array[i];
+        }
+        length--;
     }
 
     T& operator[](int index) {
-        return {};
+        if (index < 0 || index >= length) {
+            throw std::out_of_range("Subscript out of bounds");
+        }
+
+        return array[index];
     }
 
     const T& operator[](int index) const {
-        return {};
+        if (index < 0 || index >= length) {
+            throw std::out_of_range("Subscript out of bounds");
+        }
+
+        return array[index];
     }
 
     friend std::ostream &operator<<(std::ostream &strm, const CircularBuffer<T> &buffer) {
