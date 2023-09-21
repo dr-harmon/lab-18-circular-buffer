b9fa0f6d4f60999105e6bdcc96e8c234f26970c1
Trevor Harmon <tharmon@laspositascollege.edu>
2023-09-20 17:10:17 -0700

    Add solution (part 2)

--- src/circular_buffer.h
+++ src/circular_buffer.h
@@ -10,11 +10,13 @@ private:
     int maxSize;
     std::unique_ptr<T[]> array;
     int length;
+    int start;
 public:
     CircularBuffer(int maxSize) {
         this->maxSize = maxSize;
         array = std::make_unique<T[]>(maxSize);
         length = 0;
+        start = 0;
     }
 
     int getSize() const {
@@ -25,10 +27,17 @@ public:
         if (index < 0 || index > length || length == maxSize) {
             throw std::out_of_range("Cannot insert");
         }
-        for (int i = length - 1; i >= index; i--) {
-            array[i + 1] = array[i];
+        if (index == 0) {
+            start--;
+            if (start == -1) {  // Protect against underflow
+                start = maxSize - 1;
+            }
+        } else {
+            for (int i = length - 1; i >= index; i--) {
+                array[(start + i + 1) % maxSize] = array[(start + i) % maxSize];
+            }
         }
-        array[index] = element;
+        array[(start + index) % maxSize] = element;
         length++;
     }
 
@@ -36,8 +45,15 @@ public:
         if (index < 0 || index >= length) {
             throw std::out_of_range("Cannot remove");
         }
-        for (int i = index + 1; i < length; i++) {
-            array[i - 1] = array[i];
+        if (index == 0) {
+            start++;
+            if (start == maxSize) {  // Protect against overflow
+                start = 0;
+            }
+        } else {
+            for (int i = index + 1; i < length; i++) {
+                array[(start + i - 1) % maxSize] = array[(start + i) % maxSize];
+            }
         }
         length--;
     }
@@ -47,7 +63,7 @@ public:
             throw std::out_of_range("Subscript out of bounds");
         }
 
-        return array[index];
+        return array[(start + index) % maxSize];
     }
 
     const T& operator[](int index) const {
@@ -55,7 +71,7 @@ public:
             throw std::out_of_range("Subscript out of bounds");
         }
 
-        return array[index];
+        return array[(start + index) % maxSize];
     }
 
     friend std::ostream &operator<<(std::ostream &strm, const CircularBuffer<T> &buffer) {
